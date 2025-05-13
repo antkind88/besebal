0061eb915a63f995ccfade3024b878596881774d
Caleb K <antkind88@gmail.com>
2025-05-13 13:57:04 -0600

    bootstrapping

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='./baseball.png' className="App-logo" alt="logo" />
+        <h2>Baseball Players Index</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
